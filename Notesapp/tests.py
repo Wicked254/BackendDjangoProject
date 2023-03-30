77d7fdf30a2f0e258f188ba52230809133b33906
Wicked254 <gjeff193@gmail.com>
2023-03-30 23:27:14 +0300

    Second commit

--- Notesapp/tests.py
+++ Notesapp/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
 
-# Create your tests here.
+class QuestionAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_questions_list(self):
+        # Test that a GET request to /api/questions/ returns a list of questions
+        response = self.client.get('/api/questions/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, list)
